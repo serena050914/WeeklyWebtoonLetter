1d1db20c0aeb6214d7a1201dd1925c65663639dd
serena050914 <serena050914@gmail.com>
2026-02-21 03:07:06 +0900

    emailjs/nodejs로 변경함

--- backed_logic/src/scripts/sendEmail/sendEmail.ts
+++ backed_logic/src/scripts/sendEmail/sendEmail.ts
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv';
-import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/nodejs';
 import { EditedData } from './types';
+
+// .env 예시 (Vite 기준)
+// VITE_EMAILJS_SERVICE_ID=...
+// VITE_EMAILJS_TEMPLATE_ID=...
+// VITE_EMAILJS_PUBLIC_KEY=...
 
 // .env 파일의 환경 변수를 로드
 dotenv.config();
@@ -9,29 +14,22 @@ dotenv.config();
 const SERVICE_ID = process.env.VITE_EMAILJS_SERVICE_ID as string;
 const TEMPLATE_ID = process.env.VITE_EMAILJS_TEMPLATE_ID as string;
 const PUBLIC_KEY = process.env.VITE_EMAILJS_PUBLIC_KEY as string;
+const PRIVATE_KEY = process.env.VITE_EMAILJS_PRIVATE_KEY as string; // 백엔드에서만 사용하는 키, Vite 환경 변수로는 노출하지 않음
 
 // 2. 환경 변수 체크
-if (!SERVICE_ID || !TEMPLATE_ID || !PUBLIC_KEY) {
+if (!SERVICE_ID || !TEMPLATE_ID || !PUBLIC_KEY || !PRIVATE_KEY) {
   throw new Error(
-    'EmailJS env missing: VITE_EMAILJS_SERVICE_ID / VITE_EMAILJS_TEMPLATE_ID / VITE_EMAILJS_PUBLIC_KEY'
+    'EmailJS env missing: VITE_EMAILJS_SERVICE_ID / VITE_EMAILJS_TEMPLATE_ID / VITE_EMAILJS_PUBLIC_KEY/ VITE_EMAILJS_PRIVATE_KEY'
   );
 }
 
 // 3. EmailJS 초기화
-emailjs.init(PUBLIC_KEY);
+emailjs.init({
+  publicKey: PUBLIC_KEY,
+  privateKey: PRIVATE_KEY,
+});
 
-// 4. location 속이기 (Node.js 환경에서 location 객체 정의)
-if (typeof window === 'undefined') {
-  // 'location' 속성을 globalThis에 추가
-  (globalThis as any).location = {
-    pathname: '/',
-    search: '',
-    hash: '',
-    href: 'http://localhost', // URL을 원하는 대로 설정
-  };
-}
-
-// 5. 템플릿 파라미터의 타입 지정
+// 4. 템플릿 파라미터의 타입 지정
 /**
  * EmailJS 템플릿에 변수 객체를 그대로 주입해서 메일 전송
  * @param {Record<string, any>} templateParams - 템플릿의 {{...}} 변수명과 동일한 key들
